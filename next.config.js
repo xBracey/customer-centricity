35857c8f4a0b67125ef8e74ca65ffb3b85a6bc18
Thomas Brace <thomas.brace@mangahigh.com>
2021-06-26 10:36:39 +0100

    More setup

--- next.config.js
+++ next.config.js
@@ -1,27 +1,5 @@
 module.exports = {
-  webpack: config => {
-    config.module.rules.push(
-      {
-        test: /\.inline.svg$/,
-        issuer: {
-          test: /\.(js|ts)x?$/,
-        },
-        use: ["@svgr/webpack"],
-      },
-      {
-        test: /(?<!\.inline)\.(png|jpg|gif|svg)$/i,
-        use: [
-          {
-            loader: "url-loader",
-          },
-        ],
-      }
-    );
-
+  webpack: (config) => {
     return config;
-  },
-
-  publicRuntimeConfig: {
-    NODE_ENV: process.env.NODE_ENV,
   },
 };
